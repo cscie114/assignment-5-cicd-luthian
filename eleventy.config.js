8848cc59261939d6c9e9cf659ac13e4112716f91
DDA <github@dantonio.net>
2024-04-11 21:24:51 -0400

    eslint reformatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,11 @@
-module.exports = function(config) {
- 
-    config.addPassthroughCopy("src/js");
-    config.addPassthroughCopy("src/css");
+module.exports = function (config) {
+  config.addPassthroughCopy('src/js');
+  config.addPassthroughCopy('src/css');
 
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
-    };
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+  };
 };
