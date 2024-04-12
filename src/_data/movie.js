a7d9a4366d0a69cf86b1cc4dd6d1b27bb6e0e039
DDA <github@dantonio.net>
2024-04-11 21:25:13 -0400

    eslint reformatting
    - Using AirBnB style choices

--- src/_data/movie.js
+++ src/_data/movie.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
 const axios = require('axios').default;
 
-module.exports = async function() {
-
-    try {
-        const movie =
-            await axios.get("https://omdbapi.com/?i=" + process.env.MOVIE_ID + "&apiKey=" + process.env.MOVIE_API_KEY);
-        return movie.data;
-
-    } catch (error) {
-        console.error(error);
-
-    }
-
+module.exports = async function getData() {
+  try {
+    const movie = await axios.get(`https://omdbapi.com/?i=${process.env.MOVIE_ID}&apiKey=${process.env.MOVIE_API_KEY}`);
+    return movie.data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return {};
+  }
 };
