d2939325665bddb34c91986d419a3a8366b76176
DDA <github@dantonio.net>
2024-04-15 16:52:19 -0400

    Update tests to verify `addPassthroughCopy` is called properly
    - Verify params and number of calls.
    - Only call config file once and then check values.

--- test/11ty-config.test.js
+++ test/11ty-config.test.js
@@ -8,6 +8,8 @@ test('Check input and output directories in 11ty config', () => {
   // Check the return values
   expect(result.dir.input).toBe('src');
   expect(result.dir.output).toBe('dist');
-
+  // Make sure the addPassthroughCopy function was called properly
+  expect(config.addPassthroughCopy.mock.calls[0][0]).toBe('src/js');
+  expect(config.addPassthroughCopy.mock.calls[1][0]).toBe('src/css');
   expect(config.addPassthroughCopy).toHaveBeenCalledTimes(2);
 });
