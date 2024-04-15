44c74c015f1a4c2f038174284b21edc134a47f01
DDA <github@dantonio.net>
2024-04-15 16:45:25 -0400

    Update tests to count number of times `addPassthroughCopy` is called

--- test/11ty-config.test.js
+++ test/11ty-config.test.js
@@ -4,6 +4,10 @@ test('Check input and output directories in 11ty config', () => {
   // Mock out the 11ty config object
   const config = {addPassthroughCopy: jest.fn()};
   // Call the function from eleventy.config.js
-  expect(eleventyConfig(config).dir.input).toBe('src');
-  expect(eleventyConfig(config).dir.output).toBe('dist');
+  const result = eleventyConfig(config)
+  // Check the return values
+  expect(result.dir.input).toBe('src');
+  expect(result.dir.output).toBe('dist');
+
+  expect(config.addPassthroughCopy).toHaveBeenCalledTimes(2);
 });
