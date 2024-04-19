9a59ac8f76efc9441bdd6fdafcd3de2d439d50ab
DDA <github@dantonio.net>
2024-04-19 10:01:06 -0400

    Add edge function pieces

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
+const { EleventyEdgePlugin } = require("@11ty/eleventy");
+
 module.exports = function (config) {
+  config.addPlugin(EleventyEdgePlugin);
+
   config.addPassthroughCopy('src/js');
   config.addPassthroughCopy('src/css');
 
