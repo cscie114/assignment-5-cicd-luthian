028b7bdcc8e15dff3e1632d166773a095feef1a0
DDA <github@dantonio.net>
2024-04-19 14:10:20 -0400

    Make geo object global

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -3,23 +3,27 @@ import {
   precompiledAppData,
 } from "./_generated/eleventy-edge-app.js";
 
+
 export default async (request, context) => {
   try {
+    
     let edge = new EleventyEdge("edge", {
       request,
       context,
       precompiled: precompiledAppData,
-
       // default is [], add more keys to opt-in e.g. ["appearance", "username"]
-      cookies: [],
+      cookies: ["appearance", "username"]
     });
 
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
       // e.g. Fancier json output
-      // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+      eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+      eleventyConfig.addGlobalData("geo", context.geo);
     });
 
+    console.log(context.geo);
+  
     return await edge.handleResponse();
   } catch (e) {
     console.log("ERROR", { e });
