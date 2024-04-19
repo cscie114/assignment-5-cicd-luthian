397c475fc0feea5f26eb0241da5581067ff109e8
DDA <github@dantonio.net>
2024-04-19 16:31:40 -0400

    Get some data from NASA and return it and some geo data
    - Mars Curiousity Rover front hazard camera data
    - From 2021

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,32 +1,41 @@
-import {
-  EleventyEdge,
-  precompiledAppData,
-} from "./_generated/eleventy-edge-app.js";
-
+import '../../src/env.js';
+import axios from 'axios';
+import { EleventyEdge, precompiledAppData } from './_generated/eleventy-edge-app.js';
 
 export default async (request, context) => {
   try {
-    
-    let edge = new EleventyEdge("edge", {
+    // Get Mars Curiousity Rover image info for this day in 2021 ans save in global data
+    // Since the rover stopped sending images, we will use the date to get the image
+
+    const year2021 = new Date()
+    year2021.setFullYear(2021);
+    const today = year2021.toISOString().split('T')[0]; // YYYY-MM-DD;
+    const params = new URLSearchParams({ earth_date: today, camera: 'FHAZ', api_key: process.env.NASA_API_KEY });
+
+    const resp = await axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos', { params });
+    const onePhoto = resp.data.photos[0];
+
+    let edge = new EleventyEdge('edge', {
       request,
       context,
       precompiled: precompiledAppData,
       // default is [], add more keys to opt-in e.g. ["appearance", "username"]
-      cookies: ["appearance", "username"]
+      cookies: [],
     });
 
-    edge.config((eleventyConfig) => {
+    edge.config(eleventyConfig => {
       // Add some custom Edge-specific configuration
       // e.g. Fancier json output
-      eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
-      eleventyConfig.addGlobalData("geo", context.geo);
+      eleventyConfig.addFilter('json', obj => JSON.stringify(obj, null, 2));
+      eleventyConfig.addGlobalData('geo', context.geo);
+      eleventyConfig.addGlobalData('photo', onePhoto);
     });
 
-    console.log(context.geo);
-  
+    // console.log(context.geo);
+
     return await edge.handleResponse();
   } catch (e) {
-    console.log("ERROR", { e });
+    console.log('ERROR', { e });
     return context.next(e);
   }
 };
