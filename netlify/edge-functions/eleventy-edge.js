2b79467e1d3f9b843fe746763a3e1f014bb2e9e4
DDA <github@dantonio.net>
2024-04-23 21:37:08 -0400

    Remove unneeded try

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,30 +1,26 @@
 import { EleventyEdge, precompiledAppData } from './_generated/eleventy-edge-app.js';
 import { fetch } from 'node-fetch';
-// import '../../src/env.js';
+import '../../src/env.js';
 
 export default async (request, context) => {
   try {
-  // Get Mars Curiousity Rover image info for this day in 2021 ans save in global data
-  // Since the rover stopped sending images, we will use the date to get the image
+    // Get Mars Curiousity Rover image info for this day in 2021 ans save in global data
+    // Since the rover stopped sending images, we will use the date to get the image
 
-  const year2021 = new Date();
-  year2021.setFullYear(2021);
-  const today = year2021.toISOString().split('T')[0]; // YYYY-MM-DD;
+    const year2021 = new Date();
+    year2021.setFullYear(2021);
+    const today = year2021.toISOString().split('T')[0]; // YYYY-MM-DD;
 
-  const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
-  const params = new URLSearchParams({ earth_date: today, camera: 'FHAZ', api_key: process.env.NASA_API_KEY });
-  console.log('url', url, 'params', params.toString());
+    const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
+    const params = new URLSearchParams({ earth_date: today, camera: 'FHAZ', api_key: process.env.NASA_API_KEY });
+    console.log('url', url, 'params', params.toString());
 
-  try {
     const response = await fetch(`${url}/?${params.toString()}`, {
       headers: {
         Accept: 'application/json',
       },
     });
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText };
-    }
+
     const data = await response.json();
     console.log('data', data);
 
